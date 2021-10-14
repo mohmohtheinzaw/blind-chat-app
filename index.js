e2a54e1826e13aab8f9dde0681daeac07360a47b
mohmoh <mohmohtheinzaw379@gmail.com>
2021-10-15 02:52:21 +0630

    chat app

---
+++ index.js
@@ -0,0 +1,22 @@
+let express = require("express");
+let socket= require('socket.io');
+/* app setup*/
+let app=express();
+/* server setup*/
+let server = app.listen(7000, () => {
+    console.log('project is running on localhost:7000');
+});
+/** route setup */
+app.get("/",(res, req) => {
+    req.sendFile(__dirname + "/public/index.html");
+});
+/** socket setup */
+let io = socket(server);
+io.on("connection", (socket) =>{
+    socket.on("chat", (data) =>{
+        io.sockets.emit("chat",data);
+    });
+    socket.on("typing", (name) =>{
+        socket.broadcast.emit("typing",name);
+    });
+});
